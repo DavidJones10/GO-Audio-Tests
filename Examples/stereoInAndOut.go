2aa993fe9ac66a32f57bde3ca92031534e138bb9
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-11-14 11:05:53 -0500

    test with goalsa

--- Examples/stereoInAndOut.go
+++ Examples/stereoInAndOut.go
@@ -1,54 +1,19 @@
 package main
 
-import (
-	"github.com/gordonklaus/portaudio"
-)
+import "github.com/cocoonlife/goalsa"
 
-const BUFFER_SIZE = 480
 const SAMPLE_RATE = 16000
 
 func main() {
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-
-	inBuf := make([][]int16, 2) // 2 channels
-	for i := range inBuf {
-		inBuf[i] = make([]int16, BUFFER_SIZE)
+	//hw:<CARD_NR>,<DEVICE_NR>
+	bufParams := alsa.BufferParams{
+		BufferFrames: 1920,
+		PeriodFrames: 480,
+		Periods:      4,
 	}
-	inStream, err := portaudio.OpenDefaultStream(2, 0, SAMPLE_RATE, BUFFER_SIZE, inBuf)
+	captureDevice, err := alsa.NewCaptureDevice("hw:2,0", 2, alsa.FormatS16LE, SAMPLE_RATE, bufParams)
 	if err != nil {
 		panic(err)
 	}
-
-	outBuf := make([][]int16, 2) // 2 channels
-	for i := range outBuf {
-		outBuf[i] = make([]int16, BUFFER_SIZE)
-	}
-	outStream, err := portaudio.OpenDefaultStream(0, 2, SAMPLE_RATE, BUFFER_SIZE, outBuf)
-	if err != nil {
-		panic(err)
-	}
-
-	inStream.Start()
-	outStream.Start()
-
-	go processAudio(inStream, outStream, inBuf, outBuf)
-
-}
-
-func processAudio(inputStream *portaudio.Stream, outputStream *portaudio.Stream, inputBuffer [][]int16, outputBuffer [][]int16) {
-	for {
-		if err := inputStream.Read(); err != nil {
-			panic(err)
-		}
-
-		for channel := 0; channel < 2; channel++ {
-			for i := 0; i < BUFFER_SIZE; i++ {
-				outputBuffer[channel][i] = inputBuffer[channel][i] // Loop input to output
-			}
-		}
-		if err := outputStream.Write(); err != nil {
-			panic(err)
-		}
-	}
+	defer captureDevice.Close()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module Examples
 
 go 1.23.2
 
-require github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5
+require github.com/cocoonlife/goalsa v0.0.0-20230731174733-19b297509b2e
+
+require github.com/cocoonlife/testify v0.0.0-20160218172820-792cc1faeb64 // indirect
