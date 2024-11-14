bd440bcfb65e93b14cdf731286d92c31f8e1303a
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-11-14 13:35:37 -0500

    copy portaudio example

--- Examples/stereoInAndOut.go
+++ Examples/stereoInAndOut.go
@@ -1,54 +1,49 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gordonklaus/portaudio"
 )
-
-const BUFFER_SIZE = 480
-const SAMPLE_RATE = 16000
 
 func main() {
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-
-	inBuf := make([][]int16, 2) // 2 channels
-	for i := range inBuf {
-		inBuf[i] = make([]int16, BUFFER_SIZE)
-	}
-	inStream, err := portaudio.OpenDefaultStream(2, 0, SAMPLE_RATE, BUFFER_SIZE, inBuf)
-	if err != nil {
-		panic(err)
-	}
-
-	outBuf := make([][]int16, 2) // 2 channels
-	for i := range outBuf {
-		outBuf[i] = make([]int16, BUFFER_SIZE)
-	}
-	outStream, err := portaudio.OpenDefaultStream(0, 2, SAMPLE_RATE, BUFFER_SIZE, outBuf)
-	if err != nil {
-		panic(err)
-	}
-
-	inStream.Start()
-	outStream.Start()
-
-	go processAudio(inStream, outStream, inBuf, outBuf)
-
+	e := newEcho(time.Second / 3)
+	defer e.Close()
+	chk(e.Start())
+	time.Sleep(4 * time.Second)
+	chk(e.Stop())
 }
 
-func processAudio(inputStream *portaudio.Stream, outputStream *portaudio.Stream, inputBuffer [][]int16, outputBuffer [][]int16) {
-	for {
-		if err := inputStream.Read(); err != nil {
-			panic(err)
-		}
+type echo struct {
+	*portaudio.Stream
+	buffer []float32
+	i      int
+}
 
-		for channel := 0; channel < 2; channel++ {
-			for i := 0; i < BUFFER_SIZE; i++ {
-				outputBuffer[channel][i] = inputBuffer[channel][i] // Loop input to output
-			}
-		}
-		if err := outputStream.Write(); err != nil {
-			panic(err)
-		}
+func newEcho(delay time.Duration) *echo {
+	h, err := portaudio.DefaultHostApi()
+	chk(err)
+	p := portaudio.LowLatencyParameters(h.DefaultInputDevice, h.DefaultOutputDevice)
+	p.Input.Channels = 1
+	p.Output.Channels = 1
+	e := &echo{buffer: make([]float32, int(p.SampleRate*delay.Seconds()))}
+	e.Stream, err = portaudio.OpenStream(p, e.processAudio)
+	chk(err)
+	return e
+}
+
+func (e *echo) processAudio(in, out []float32) {
+	for i := range out {
+		out[i] = .7 * e.buffer[e.i]
+		e.buffer[e.i] = in[i]
+		e.i = (e.i + 1) % len(e.buffer)
+	}
+}
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
