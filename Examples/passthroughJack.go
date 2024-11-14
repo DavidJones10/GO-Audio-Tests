93dd36d697f0aa7292b324d7965cb30d79dc021c
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-11-14 16:09:14 -0500

    try connecting other clients

--- Examples/passthroughJack.go
+++ Examples/passthroughJack.go
@@ -12,48 +12,68 @@ var PortsIn []*jack.Port
 var PortsOut []*jack.Port
 
 func process(nframes uint32) int {
+	count := 0
 	for i, in := range PortsIn {
 		samplesIn := in.GetBuffer(nframes)
 		samplesOut := PortsOut[i].GetBuffer(nframes)
 		for i2, sample := range samplesIn {
 			samplesOut[i2] = sample
+			count += 1
 		}
 	}
+	fmt.Println("Samples Processed: %v", count)
 	return 0
 }
 
 func main() {
-	client, status := jack.ClientOpen("Go Passthrough", jack.NoStartServer)
+	myClient, status := jack.ClientOpen("Go Passthrough", jack.NoStartServer)
 	if status != 0 {
 		fmt.Println("Status:", jack.StrError(status))
 		return
 	}
-	defer client.Close()
+	defer myClient.Close()
 
-	for i := 0; i < channels; i++ {
-		portIn := client.PortRegister(fmt.Sprintf("in_%d", i), jack.DEFAULT_AUDIO_TYPE, jack.PortIsInput, 0)
-		PortsIn = append(PortsIn, portIn)
+	echoCancelSource, status := jack.ClientOpen("Echo-Cancel Source", jack.UseExactName)
+	if status != 0 {
+		fmt.Println("Status:", jack.StrError(status))
+		return
 	}
-	for i := 0; i < channels; i++ {
-		portOut := client.PortRegister(fmt.Sprintf("out_%d", i), jack.DEFAULT_AUDIO_TYPE, jack.PortIsOutput, 0)
-		PortsOut = append(PortsOut, portOut)
-	}
+	defer echoCancelSource.Close()
 
-	if code := client.SetProcessCallback(process); code != 0 {
+	echoCancelSink, status := jack.ClientOpen("Echo-Cancel Sink", jack.UseExactName)
+	if status != 0 {
+		fmt.Println("Status:", jack.StrError(status))
+		return
+	}
+	defer echoCancelSink.Close()
+
+	portIn1 := myClient.PortRegister("Lane1Input", jack.DEFAULT_AUDIO_TYPE, jack.PortIsInput, 0)
+	portIn2 := myClient.PortRegister("Lane2Input", jack.DEFAULT_AUDIO_TYPE, jack.PortIsInput, 0)
+	PortsIn = append(PortsIn, portIn1, portIn2)
+	myClient.ConnectPorts(echoCancelSource.GetPortByName("capture_FL"), portIn1)
+	myClient.ConnectPorts(echoCancelSource.GetPortByName("capture_FR"), portIn2)
+
+	portOut1 := myClient.PortRegister("Lane_1_Output", jack.DEFAULT_AUDIO_TYPE, jack.PortIsOutput, 0)
+	portOut2 := myClient.PortRegister("Lane_2_Output", jack.DEFAULT_AUDIO_TYPE, jack.PortIsOutput, 0)
+	PortsOut = append(PortsOut, portOut1, portOut2)
+	myClient.ConnectPorts(echoCancelSink.GetPortByName("playback_FL"), portIn1)
+	myClient.ConnectPorts(echoCancelSink.GetPortByName("playback_FR"), portIn2)
+
+	if code := myClient.SetProcessCallback(process); code != 0 {
 		fmt.Println("Failed to set process callback:", jack.StrError(code))
 		return
 	}
 	shutdown := make(chan struct{})
-	client.OnShutdown(func() {
+	myClient.OnShutdown(func() {
 		fmt.Println("Shutting down")
 		close(shutdown)
 	})
 
-	if code := client.Activate(); code != 0 {
+	if code := myClient.Activate(); code != 0 {
 		fmt.Println("Failed to activate client:", jack.StrError(code))
 		return
 	}
 
-	fmt.Println(client.GetName())
+	fmt.Println(myClient.GetName())
 	<-shutdown
 }
