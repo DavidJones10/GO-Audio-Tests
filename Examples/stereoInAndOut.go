6e9281128159dcbe99c257e3cb56c7fdcd9c2664
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-11-14 09:44:07 -0500

    Initial Commit

---
+++ Examples/stereoInAndOut.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gordonklaus/portaudio"
+)
+
+const BUFFER_SIZE = 480
+const SAMPLE_RATE = 16000
+
+func main() {
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+	inBuf := make([][]int16, BUFFER_SIZE)
+	inStream, err := portaudio.OpenDefaultStream(2, 0, SAMPLE_RATE, BUFFER_SIZE, inBuf)
+	if err != nil {
+		panic(err)
+	}
+
+	outBuf := make([][]int16, BUFFER_SIZE)
+	outStream, err := portaudio.OpenDefaultStream(0, 2, SAMPLE_RATE, BUFFER_SIZE, outBuf)
+	if err != nil {
+		panic(err)
+	}
+
+	inStream.Start()
+	outStream.Start()
+
+	go processAudio(inStream, outStream, inBuf, outBuf)
+
+}
+
+func processAudio(inputStream *portaudio.Stream, outputStream *portaudio.Stream, inputBuffer [][]int16, outputBuffer [][]int16) {
+	for {
+		if err := inputStream.Read(); err != nil {
+			panic(err)
+		}
+
+		for channel := 0; channel < 2; channel++ {
+			for i := 0; i < BUFFER_SIZE; i++ {
+				outputBuffer[channel][i] = inputBuffer[channel][i] // Loop input to output
+			}
+		}
+		if err := outputStream.Write(); err != nil {
+			panic(err)
+		}
+	}
+}
