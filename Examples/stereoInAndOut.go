93f7792ce04bc662f9de461fecdabd1b0eaa73da
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-11-14 10:06:43 -0500

    Return to stereo

--- Examples/stereoInAndOut.go
+++ Examples/stereoInAndOut.go
@@ -11,14 +11,14 @@ func main() {
 	portaudio.Initialize()
 	defer portaudio.Terminate()
 
-	inBuf := make([]int16, BUFFER_SIZE)
-	inStream, err := portaudio.OpenDefaultStream(1, 0, SAMPLE_RATE, BUFFER_SIZE, inBuf)
+	inBuf := make([][]int16, BUFFER_SIZE)
+	inStream, err := portaudio.OpenDefaultStream(2, 0, SAMPLE_RATE, BUFFER_SIZE, inBuf)
 	if err != nil {
 		panic(err)
 	}
 
-	outBuf := make([]int16, BUFFER_SIZE)
-	outStream, err := portaudio.OpenDefaultStream(0, 1, SAMPLE_RATE, BUFFER_SIZE, outBuf)
+	outBuf := make([][]int16, BUFFER_SIZE)
+	outStream, err := portaudio.OpenDefaultStream(0, 2, SAMPLE_RATE, BUFFER_SIZE, outBuf)
 	if err != nil {
 		panic(err)
 	}
@@ -30,15 +30,15 @@ func main() {
 
 }
 
-func processAudio(inputStream *portaudio.Stream, outputStream *portaudio.Stream, inputBuffer []int16, outputBuffer []int16) {
+func processAudio(inputStream *portaudio.Stream, outputStream *portaudio.Stream, inputBuffer [][]int16, outputBuffer [][]int16) {
 	for {
 		if err := inputStream.Read(); err != nil {
 			panic(err)
 		}
 
-		for channel := 0; channel < 1; channel++ {
+		for channel := 0; channel < 2; channel++ {
 			for i := 0; i < BUFFER_SIZE; i++ {
-				outputBuffer[i] = inputBuffer[i] // Loop input to output
+				outputBuffer[channel][i] = inputBuffer[channel][i] // Loop input to output
 			}
 		}
 		if err := outputStream.Write(); err != nil {
